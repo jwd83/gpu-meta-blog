5df87c30f01216bdc1d5b19530387f786d746fbb
Jared De Blander <jared0x90@gmail.com>
2023-06-19 01:14:21 -0400

    updated data and added a sources page

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -7,6 +7,7 @@ import * as $0 from "./routes/_app.tsx";
 import * as $1 from "./routes/gpu/[gpu_name].tsx";
 import * as $2 from "./routes/gpu/index.tsx";
 import * as $3 from "./routes/index.tsx";
+import * as $4 from "./routes/sources.tsx";
 import * as $$0 from "./islands/Counter.tsx";
 
 const manifest = {
@@ -15,6 +16,7 @@ const manifest = {
     "./routes/gpu/[gpu_name].tsx": $1,
     "./routes/gpu/index.tsx": $2,
     "./routes/index.tsx": $3,
+    "./routes/sources.tsx": $4,
   },
   islands: {
     "./islands/Counter.tsx": $$0,
---
+++ routes/sources.tsx
@@ -0,0 +1,27 @@
+const sources = [
+  "https://www.pcpartpicker.com/",
+  "https://www.techspot.com/article/2667-gpu-pricing-update/",
+  "https://www.techpowerup.com/gpu-specs/geforce-rtx-3060-12-gb.c3682",
+  "https://www.ebay.com/",
+  "https://averagefinder.com/averageFinder.php",
+  "https://reddit.com/r/hardwareswap",
+  "https://en.wikipedia.org/wiki/GeForce_30_series",
+  "https://en.wikipedia.org/wiki/GeForce_20_series",
+  "https://en.wikipedia.org/wiki/GeForce_10_series",
+  "https://en.wikipedia.org/wiki/Radeon_RX_6000_series",
+];
+
+export default function Sources() {
+  return (
+    <div class="text-center">
+      <h1 class="text-4xl my-4">Sources</h1>
+      <ul>
+        {sources.map((source) => (
+          <li>
+            <a href="{source}">{source}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
