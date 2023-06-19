807666aa5a508cf922610c076ceb95ee4ec799f8
Jared De Blander <jared0x90@gmail.com>
2023-06-18 23:34:49 -0400

    added simple performance graph

--- routes/gpu/index.tsx
+++ routes/gpu/index.tsx
@@ -1,7 +1,26 @@
 // import the gpu data from /data/gpu_list.js
 import { gpu_list } from "$app/data/gpu_list.js";
 
+const max_width = 100;
+
+function get_max_gpu_relative_performance(): number {
+  // find the maximum relative performance in the gpu_list
+  let max_gpu_perf = 0;
+
+  for (let i = 0; i < gpu_list.length; i++) {
+    const gpu_perf = parseInt(gpu_list[i]["Relative Performance"]);
+
+    if (gpu_perf > max_gpu_perf) {
+      max_gpu_perf = gpu_perf;
+    }
+  }
+
+  return max_gpu_perf;
+}
+
 export default function GPUsPage() {
+  const max_gpu_perf = get_max_gpu_relative_performance();
+
   return (
     <>
       <h1 class="text-center text-4xl my-4">GPUs</h1>
@@ -32,7 +51,20 @@ export default function GPUsPage() {
                 </a>
               </td>
               {/* <td class="text-center">{gpu.Architecture}</td> */}
-              <td class="text-center">{gpu["Relative Performance"]}</td>
+              {/* <td class="text-center"></td> */}
+              <td class="text-left">
+                <img
+                  src="data:image/gif;base64,R0lGODlhAQABAIAAAJ/ElAAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+                  height="10"
+                  width={Math.round(
+                    parseInt(gpu["Relative Performance"]) / max_gpu_perf *
+                      max_width,
+                  )}
+                  style="height: 10px;"
+                  class="inline-block"
+                />{" "}
+                {gpu["Relative Performance"]}
+              </td>
               <td class="text-right">{gpu["Launch MSRP"]}</td>
               <td class="text-right">{gpu["Recent New Pricing"]}</td>
               <td class="text-right">{gpu["Recent Used Pricing"]}</td>
